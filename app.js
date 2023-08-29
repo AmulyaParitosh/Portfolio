2718d3887383283e5bc093776a0f2cc80e29bb50
Amulya Paritosh <amulya1171@gmail.com>
2023-08-30 02:31:27 +0530

    added image and info

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-const section = document.querySelectorAll('.section');
+const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelectorAll('.main-content');
 
 function pageTransitions() {
-	for (let i = 0; i < sectBtn.length; i++){
-		sectBtn[i].addEventListener('click', function(){
+	sectBtn.forEach((btn) => {
+		btn.addEventListener('click', function () {
 			let currentBtn = document.querySelectorAll('.active-btn')[0];
-			currentBtn.className = currentBtn.className.replace(' active-btn', '');
-			this.className += ' active-btn';
+			currentBtn.classList.remove('active-btn');
+			this.classList.add('active-btn');
 		})
-	}
+	})
+
+	allSections.forEach((sec) => {
+		sec.addEventListener('click', (e) => {
+			const id = e.target.dataset.id;
+			if (id) {
+				// sectBtns.forEach((btn) => {
+				// 	btn.classList.remove('active')
+				// })
+				// e.target.classList.add('active')
+
+				sections.forEach((section) => {
+					section.classList.remove('active')
+				})
+
+				const element = document.getElementById(id);
+				element.classList.add('active');
+			}
+		})
+	})
 }
 
 pageTransitions();
